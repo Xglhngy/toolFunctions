1846e4219cd936cbb3fbc677773a818471ff85d6
18538527335 <1498952582@qq.com>
2022-11-25 11:07:08 +0800

    提交文件位置

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,10 @@ export const dayOfYear = (date?: Date | string): number => {
   return Math.floor(timeGap / 1000 / 60 / 60 / 24);
 };
 
-import * as date from '../components/date'
-import * as file from '../components/file'
-import * as check from '../components/check'
-import * as filter from '../components/filter'
+import * as date from './components/date'
+import * as file from './components/file'
+import * as check from './components/check'
+import * as filter from './components/filter'
 export const xyd_date = {...date}
 export const xyd_file = { ...file }
 export const xyd_check = { ...check }
